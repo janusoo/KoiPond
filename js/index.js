5a0b1b32089baea23ad1cdeb864678346375c733
Kent Miller <janusoo@gmail.com>
2016-08-18 15:57:23 -0400

    Made minor js updates to streamline scripts. Addednew SVGs to HTML file. Updated css file.

--- js/index.js
+++ js/index.js
@@ -1,21 +1,3 @@
-// function getCoords(elem) { // crossbrowser version
-//     var box = elem.getBoundingClientRect();
-
-//     var body = document.body;
-//     var docEl = document.documentElement;
-
-//     var scrollTop = window.pageYOffset || docEl.scrollTop || body.scrollTop;
-//     var scrollLeft = window.pageXOffset || docEl.scrollLeft || body.scrollLeft;
-
-//     var clientTop = docEl.clientTop || body.clientTop || 0;
-//     var clientLeft = docEl.clientLeft || body.clientLeft || 0;
-
-//     var top  = box.top +  scrollTop - clientTop;
-//     var left = box.left + scrollLeft - clientLeft;
-
-//     return { top: Math.round(top), left: Math.round(left) };
-// }
-
 // $('window').on('click', setNewCoord());
 
 var windowWidth = $(window).width();
@@ -28,41 +10,49 @@ var xCoord;
 function setKoiCoord() {
 	yCoord = Math.random() * 100 + "%";
 	xCoord = Math.random() * 100 + "%";
-	$('#koiSvg1').css({'top' : yCoord , 'left' : xCoord, 'display': 'inline'} );
+	$('.koi-svg').css({'top' : yCoord , 'left' : xCoord, 'display': 'inline'} );
 	// setNewCoord();
 }
 
 function getCoords(e) {
-	var currentX = $('#koiSvg1').offset().left;
-	var currentY = $('#koiSvg1').offset().top;
+	var currentX = $('.koi-svg').offset().left;
+	var currentY = $('.koi-svg').offset().top;
 	var destX = e.pageX;
 	var destY = e.pageY;
 
 	var angleDeg = Math.atan2(destY - currentY, destX - currentX) * 180 / Math.PI;
 
-    // $('#koiSvg1').css({'transform' : 'rotate(' + angleDeg + 'deg)', 'top' : e.pageY + 'px', 'left' : e.pageX + 'px', 'transition' : 'top 3s, left 3s'})
-    $('#koiSvg1').css({'transform' : 'rotate(' + angleDeg + 'deg)', 'top' : e.pageY + 'px', 'left' : e.pageX + 'px', 'transition' : 'top 3s, left 3s, transform 3s'})
+  $('.koi-svg').css({'transform' : 'rotate(' + angleDeg + 'deg)', 'top' : e.pageY + 'px', 'left' : e.pageX + 'px', 'transition' : 'top 3s, left 3s, transform 3s'})
 }
 
 function setNewCoord() {
 	yCoord = Math.random() * 100 + '%';
 	xCoord = Math.random() * 100 + '%';
 
-	var currentX = $('#koiSvg1').offset().left;
-	var currentY = $('#koiSvg1').offset().top;
+	var currentX = $('.koi-svg').offset().left;
+	var currentY = $('.koi-svg').offset().top;
 	var destX = xCoord;
 	var destY = yCoord;
 
 	var angleDeg = Math.atan2(destY - currentY, destX - currentX) * 180 / Math.PI;
 
-	$('#koiSvg1').css({'transform' : 'rotate(' + angleDeg + 'deg)', 'top' : yCoord , 'left' : xCoord, 'transition' : 'top 20s, left 20s, transform 3s'});
+	$('.koi-svg').css({'transform' : 'rotate(' + angleDeg + 'deg)', 'top' : yCoord , 'left' : xCoord, 'transition' : 'top 20s, left 20s, transform 3s'});
+}
+
+function svgLoaded() {
+	setKoiCoord();
+	setTimeout(setNewCoord, 10);
 }
 
 $(document).ready(function() {
-	setKoiCoord();
-	setTimeout(setNewCoord, 10);
+	// setKoiCoord();
+	// setTimeout(setNewCoord, 10);
+	$('#koiSvg1').on('load', svgLoaded);
+	$('#koiSvg2').on('load', svgLoaded);
+	$('#koiSvg3').on('load', svgLoaded);
 
 	$('html').on('click', getCoords);
 
-	$('#koiSvg1').on('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', setNewCoord);
+	// $('.koi-svg').on('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', setNewCoord);
+	$('.koi-svg').on('transitionend', setNewCoord);
 })

15a345bdb206293624993a00873c5fd18a701abe
Kent Miller <janusoo@gmail.com>
2016-08-19 07:01:46 -0400

    Now 3 fish! They move ove in random independent directions, but their transition times are still synched. Will attempte to unsynchronize their timings.

--- js/index.js
+++ js/index.js
@@ -1,4 +1,7 @@
 // $('window').on('click', setNewCoord());
+var fishSelectors = ['#koiSvg1', '#koiSvg2', '#koiSvg3'];
+var fishClassSelector = '.koi-svg';
+var n = 0;
 
 var windowWidth = $(window).width();
 var windowHeight = $(window).height();
@@ -7,10 +10,10 @@ var windowDimensions = windowWidth + ' x ' + windowHeight;
 var yCoord;
 var xCoord;
 
-function setKoiCoord() {
+function setKoiCoord(selector) {
 	yCoord = Math.random() * 100 + "%";
 	xCoord = Math.random() * 100 + "%";
-	$('.koi-svg').css({'top' : yCoord , 'left' : xCoord, 'display': 'inline'} );
+	$(selector).css({'top' : yCoord , 'left' : xCoord, 'display': 'inline'} );
 	// setNewCoord();
 }
 
@@ -25,34 +28,58 @@ function getCoords(e) {
   $('.koi-svg').css({'transform' : 'rotate(' + angleDeg + 'deg)', 'top' : e.pageY + 'px', 'left' : e.pageX + 'px', 'transition' : 'top 3s, left 3s, transform 3s'})
 }
 
-function setNewCoord() {
+function setNewAll(selectors){
+	console.log("firing")
+	for (var i = 0; i < selectors.length; i++) {
+		var selector = selectors[i];
+		setNewCoord(selector);
+	}
+}
+
+function initialPositionFish(selectors) {
+	for (var i = 0; i < selectors.length; i++) {
+		var selector = selectors[i];
+		svgLoaded(selector);
+	}
+}
+
+function setNewCoord(selector) {
+	n++;
+	console.log("set new Coord firing for the " + n + "th time");
 	yCoord = Math.random() * 100 + '%';
 	xCoord = Math.random() * 100 + '%';
 
-	var currentX = $('.koi-svg').offset().left;
-	var currentY = $('.koi-svg').offset().top;
+	var currentX = $(selector).offset().left + 225;
+	var currentY = $(selector).offset().top + 63.63;
 	var destX = xCoord;
 	var destY = yCoord;
 
 	var angleDeg = Math.atan2(destY - currentY, destX - currentX) * 180 / Math.PI;
 
-	$('.koi-svg').css({'transform' : 'rotate(' + angleDeg + 'deg)', 'top' : yCoord , 'left' : xCoord, 'transition' : 'top 20s, left 20s, transform 3s'});
+	var mathRandom = Math.random() * 50;
+
+	$(selector).css({'transform' : 'rotate(' + angleDeg + 'deg)', 'top' : yCoord , 'left' : xCoord, 'transition' : 'top ' + mathRandom + 's, left ' + mathRandom + 's, transform ' + mathRandom + 's'});
 }
 
-function svgLoaded() {
-	setKoiCoord();
-	setTimeout(setNewCoord, 10);
+function svgLoaded(selector) {
+	console.log("svg loaded firing")
+	setKoiCoord(selector);
+	setTimeout(setNewCoord(selector), 10);
+}
+
+function transitionListener(fishClass) {
+		$(fishClass).on('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function() {
+			setNewAll(fishSelectors)}
+		);
 }
 
 $(document).ready(function() {
 	// setKoiCoord();
 	// setTimeout(setNewCoord, 10);
-	$('#koiSvg1').on('load', svgLoaded);
-	$('#koiSvg2').on('load', svgLoaded);
-	$('#koiSvg3').on('load', svgLoaded);
+	initialPositionFish(fishSelectors);
 
 	$('html').on('click', getCoords);
 
 	// $('.koi-svg').on('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', setNewCoord);
-	$('.koi-svg').on('transitionend', setNewCoord);
+	transitionListener(fishClassSelector);
 })
